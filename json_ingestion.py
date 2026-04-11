07bc5007ae13c877da8f16c4b62427f57ad62220
Anvesh rao <anveshrao7@gmail.com>
2026-04-11 22:00:18 +0530

    JSON ingestion using schema-based approach

--- json_ingestion.py
+++ json_ingestion.py
@@ -1,40 +1,24 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
+
 spark = SparkSession.builder \
     .appName("JSON Ingestion Project") \
     .getOrCreate()
 
-df = spark.read.option("multiline", "true") \
-    .json("data/emp3.json")
-
-df.show(5)
-df.printSchema()
-
-df.write.mode("overWrite").parquet("output/json_parquet")
-print("Task completed successfully!")
-
-
-from pyspark.sql.functions import col, explode
+schema = spark.read.json("data/schema.json").schema
+df=spark.read.schema(schema).json("data/emp3.json")
+#df.show()
 
 flat_df = df.select(
     col("__PublishDate"),
     col("events.beaconType"),
     col("events.beaconVersion"),
     col("events.client"),
-
-    # nested struct
     col("events.data.displayAd.amazonA9HB"),
     col("events.data.displayAd.inView"),
-
-    # milestones
     col("events.data.milestones.adRequested"),
-    col("events.data.milestones.amazonA9BidsReceived"),
+    col("events.data.milestones.amazonA9BidsReceived"),)
 
-    # headers (array → explode)
-    explode(col("headers.Accept")).alias("Accept_header")
-)
+flat_df.show(5,False)
 
-flat_df.show(5)
-flat_df.printSchema()
-
-flat_df.write.mode("overwrite").parquet("output/json_flattened")
-
+#flat_df.write.mode("overwrite").parquet("output/json_flattened")
