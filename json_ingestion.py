f6a7195c447a06123d3684c76d1c254bb5114f9e
Anvesh rao <anveshrao7@gmail.com>
2026-04-18 15:20:01 +0530

    Added column-level data profiling for JSON dataset

--- json_ingestion.py
+++ json_ingestion.py
@@ -23,4 +23,25 @@ flat_df.show(5,False)
 
 #flat_df.write.mode("overwrite").parquet("output/json_flattened")
 
+#flatten logic above
 
+from pyspark.sql.functions import col, count, when, countDistinct, lit
+cols = [
+    "__PublishDate","beaconType","beaconVersion","client","amazonA9HB","inView", "adRequested","amazonA9BidsReceived"]
+total_count = flat_df.count()
+dfs = []
+for c in cols:
+    df = flat_df.select(
+        lit(c).alias("col_name"),
+        lit(total_count).alias("total_record_count"),
+        count(when(col(c).isNull(), 1)).alias("null_record_count"),
+        lit(total_count).alias("source_record_count"),
+        (lit(total_count) - countDistinct(col(c))).alias("duplicate_count")
+    )
+    dfs.append(df)
+
+profile_df = dfs[0]
+for d in dfs[1:]:
+    profile_df = profile_df.unionByName(d)
+
+profile_df.show()
